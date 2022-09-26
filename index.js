bded07fd60d21bc0880734687ed4117ff442f999
Rameez <muhammadrameezahmed73@gmail.com>
2022-09-26 11:33:16 +0500

    first commit

--- index.js
+++ index.js
@@ -13,13 +13,108 @@ mongoose.connect("mongodb://127.0.0.1:27017/ram",{ useNewUrlParser: true,useUnif
 
 // Schema Create
 const teacher= new mongoose.Schema({
-    name:String,
-    salary:Number,
+    Name:String,
+    Age:Number,
     Grade:String,
     workout:Boolean
 
 })
 
-
 // collection create
 const Teacher = new mongoose.model("Teacher",teacher)
+
+
+
+
+// Documents create
+const createDocument= async ()=>{
+    try{
+        const maleteacher= new Teacher({
+            Name:"AHMED",
+            Age:22,
+            Grade:"A",
+            workout:true
+        })
+        const femaleteacher= new Teacher({
+            Name:"Amna",
+            Age:23,
+            Grade:"B",
+            workout:false
+        })
+        const oldteacher= new Teacher({
+            Name:"Aslam",
+            Age:88,
+            Grade:"A+",
+            workout:true
+        })
+        const youngteacher= new Teacher({
+            Name:"Yamen",
+            Age:99,
+            Grade:"C",
+            workout:false
+        })
+        const result =await Teacher.insertMany([maleteacher,femaleteacher,oldteacher,youngteacher]);
+        console.log(result);
+
+    }catch(error){
+        console.log(error)
+    }
+
+}
+
+
+
+
+// Document Read
+const getDocument =async ()=>{
+    try{
+        const getresult = await Teacher
+        .find({Name:"Yamen"})
+        // .select({salary:1})
+        // .limit(1);
+        console.log(getresult)
+    }catch(error){
+        console.log(error)
+    }
+
+
+    }
+  
+
+
+
+// Document Update
+const updateDocument = async ()=>{
+    try{
+        const resultupdate =await Teacher.findByIdAndUpdate({_id:"6330c106a03cf44da8077c20"},{$set:{Name:"Iftikhar"}})
+        console.log(resultupdate)
+    }catch(error){
+        console.log(error)
+
+    }
+
+}
+
+
+
+
+
+// Document delete
+const deleteDocument = async ()=>{
+    try{
+        const result= await Teacher.deleteOne({Age:22})
+   
+        console.log(result)
+    
+    }catch(error){
+        console.log(error)
+    }
+
+}
+
+
+
+// createDocument();
+// getDocument()
+// updateDocument();
+// deleteDocument()
